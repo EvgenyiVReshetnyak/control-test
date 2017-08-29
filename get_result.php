efc9d529d5ae4af54906fc6c0ccd433be570287a
EvgenyiVReshetnyak <starbreeze.rus@gmail.com>
2017-08-29 21:55:26 +0300

    translated

--- get_result.php
+++ get_result.php
@@ -2,25 +2,24 @@
 <meta charset = "utf-8">
 
 <?php
-    error_reporting(E_ALL); //вкл сообщения об ошибках
 
-    $n = 10;    //число вопросов
-    $p = 7;     //порог сдачи теста
-    $i = 0;     //счетчик ответов
+    $n = 10;    //number of questions
+    $p = 7;     //correct answers
+    $i = 0;     //counter
 
-    $keys = file('keys.txt',FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);   //читаем содержимое файла с ключами и помещаем его в массив
+    $keys = file('keys.txt',FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);   
 
-    foreach ($keys as $key){      //обходим массив keys с помощью цикла
-        if (in_array($key, $_GET)){  //ищем правильный ответ в массиве $_GET
-            $i++;                        //увеличиваем счетчик ответов на 1
+    foreach ($keys as $key){      
+        if (in_array($key, $_GET)){
+            $i++;                        
         }
     }
 
-    if ($i < $p){   //если счетчик ответов меньше порога сдачи
-        echo "<div style=\"font:bold 18px Arial; color:red;\">К сожалению, Вы не сдали тест. Попробуйте снова!</div>";  //выводим сообщение, что тест не сдан
+    if ($i < $p){
+        echo "<div style=\"font:bold 18px Arial; color:red;\">Unfortunately, you didn't pass the test. Try it again!</div>";
     }
     else{
-        echo "<div style=\"font:bold 18px Arial; color:green;\">Поздравляем! Вы успешно сдали тест!</div>";             //выводим сообщение, что тест сдан
+        echo "<div style=\"font:bold 18px Arial; color:green;\">Congratulations! You passed the test!</div>";
     }
 ?>
-<p><a href="control_test.html"><< Назад</a></p>
+<p><a href="control_test.html"><< back</a></p>
